0b49e7ef0558097fe02a6393dcc147261313b332
roshnibhuva06 <roshnibhuva6@gmail.com>
2025-09-17 12:37:12 +0530

    completed

---
+++ src/Slice/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+
+});
+export const selectCurrentUser = (state) => state.user.user;
+export default userSlice.reducer;
